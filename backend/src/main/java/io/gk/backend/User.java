867b4bf7f46a654ccfd31cac92a9b99fd4d01405
Girish <girish.pathak@publicissapient.com>
2022-05-08 17:43:42 +0530

    first commit

---
+++ backend/src/main/java/io/gk/backend/User.java
@@ -0,0 +1,25 @@
+package io.gk.backend;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+    private String email;
+    private String password;
+
+}
---
+++ backend/src/main/java/io/gk/backend/UserController.java
@@ -0,0 +1,60 @@
+package io.gk.backend;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+@EnableSwagger2
+public class UserController {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Integer id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping("/")
+    public User postUser(@RequestBody User user) {
+        return userRepository.save(user);
+
+    }
+
+    @PutMapping("/")
+    public User putUser(@RequestBody User user){
+        User oldUser = userRepository.findById(user.getId()).orElse(null);
+        oldUser.setName(user.getName());
+        oldUser.setEmail(user.getEmail());
+        oldUser.setPassword(user.getPassword());
+        return userRepository.save(oldUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public Integer deleteUser(@PathVariable Integer id) {
+        userRepository.deleteById(id);
+        return id;
+    }
+
+
+}
